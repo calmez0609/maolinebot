05b2412ac540cbe34501571eacad10febb781187
calmez0609 <33241538+calmez0609@users.noreply.github.com>
2020-01-11 00:39:11 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,15 +1,3 @@
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
 import (
@@ -26,7 +14,7 @@ var bot *linebot.Client
 
 func main() {
 	var err error
-	bot, err = linebot.New(os.Getenv("12ee5d760d086b164d6a70d29bea7313"), os.Getenv("5JMH9WPjDTX57z2U/T82VoUtd+dwGInpa/vQf9jB6GDeglvmmk40m6Jxj/O6VxgeQkCQ0ozAX/E3nJh8O7oAEhub0g2eXvV0mLPMlGAyrntFIPb1tDk+cX2+wQPHX0jU54UBMsDcKkoNowKV2EJV9QdB04t89/1O/w1cDnyilFU="))
+	bot, err = linebot.New(os.Getenv("ChannelSecret"), os.Getenv("ChannelAccessToken"))
 	log.Println("Bot:", bot, " err:", err)
 	http.HandleFunc("/callback", callbackHandler)
 	port := os.Getenv("PORT")
