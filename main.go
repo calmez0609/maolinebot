0ee75154b060ba56efdab538c8d1bb75d8a2192b
calmez0609 <33241538+calmez0609@users.noreply.github.com>
2020-01-11 00:23:13 +0800

    Update main.go

--- main.go
+++ main.go
@@ -13,47 +13,51 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
+var bot *linebot.Client
+
 func main() {
-	bot, err := linebot.New(
-		os.Getenv("12ee5d760d086b164d6a70d29bea7313"),
-		os.Getenv("5JMH9WPjDTX57z2U/T82VoUtd+dwGInpa/vQf9jB6GDeglvmmk40m6Jxj/O6VxgeQkCQ0ozAX/E3nJh8O7oAEhub0g2eXvV0mLPMlGAyrntFIPb1tDk+cX2+wQPHX0jU54UBMsDcKkoNowKV2EJV9QdB04t89/1O/w1cDnyilFU="),
-	)
+	var err error
+	bot, err = linebot.New(os.Getenv("12ee5d760d086b164d6a70d29bea7313"), os.Getenv("5JMH9WPjDTX57z2U/T82VoUtd+dwGInpa/vQf9jB6GDeglvmmk40m6Jxj/O6VxgeQkCQ0ozAX/E3nJh8O7oAEhub0g2eXvV0mLPMlGAyrntFIPb1tDk+cX2+wQPHX0jU54UBMsDcKkoNowKV2EJV9QdB04t89/1O/w1cDnyilFU="))
+	log.Println("Bot:", bot, " err:", err)
+	http.HandleFunc("/callback", callbackHandler)
+	port := os.Getenv("PORT")
+	addr := fmt.Sprintf(":%s", port)
+	http.ListenAndServe(addr, nil)
+}
+
+func callbackHandler(w http.ResponseWriter, r *http.Request) {
+	events, err := bot.ParseRequest(r)
+
 	if err != nil {
-		log.Fatal(err)
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
+		}
+		return
 	}
 
-	// Setup HTTP Server for receiving requests from LINE platform
-	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
-		events, err := bot.ParseRequest(req)
-		if err != nil {
-			if err == linebot.ErrInvalidSignature {
-				w.WriteHeader(400)
-			} else {
-				w.WriteHeader(500)
-			}
-			return
-		}
-		for _, event := range events {
-			if event.Type == linebot.EventTypeMessage {
-				switch message := event.Message.(type) {
-				case *linebot.TextMessage:
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
-						log.Print(err)
-					}
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				quota, err := bot.GetMessageQuota().Do()
+				if err != nil {
+					log.Println("Quota err:", err)
+				}
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK! remain message:"+strconv.FormatInt(quota.Value, 10))).Do(); err != nil {
+					log.Print(err)
 				}
 			}
 		}
-	})
-	// This is just sample code.
-	// For actual use, you must support HTTPS by using `ListenAndServeTLS`, a reverse proxy or something else.
-	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
-		log.Fatal(err)
 	}
 }
